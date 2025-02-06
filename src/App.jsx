a782e7ac57361d7c67987de0491b949b21e787d3
Gökay Baz <gokaybaz2000@outlook.com>
2025-02-06 15:37:23 +0300

    [Designed] Hero Section %40 Designed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { IoMdReturnRight } from 'react-icons/io';
 import './App.css'
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
@@ -9,6 +10,15 @@ function App() {
     <div className='bg-[#f2f0f1] w-screen h-screen'>
       <Header />
       <Navbar />
+      <div className='p-16 px-36'>
+        <div className='w-[600px] flex flex-col h-[340px] justify-around align-middle'>
+        <div>
+          <h2 className='text-6xl font-extrabold'>TARZINIZA UYGUN <br /> KIYAFETLER BULUN</h2>
+          <p className='mt-8 text-lg text-gray-600'>Bireyselliğinizi ortaya çıkarmak ve stil anlayışınıza hitap etmek için tasarlanmış, titizlikle hazırlanmış çeşitli giysi yelpazemize göz atın.</p>
+        </div>
+        <button className='bg-black cursor-pointer w-max px-10 py-5 rounded-full text-white font-bold'>Keşfetmeye Başla</button>
+        </div>
+      </div>
     </div>
   )
 }
